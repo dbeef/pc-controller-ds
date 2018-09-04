3e23e75baf521f13ac57bd36d96c1eed79f3464c
d.zalega <daniel.zalega@gmail.com>
2018-09-04 19:49:27 +0200

    Adding comments.

--- desktop-client/desktop_client.py
+++ desktop-client/desktop_client.py
@@ -3,9 +3,8 @@ import struct
 
 import pyautogui
 
-nintendo_ip = "10.42.0.251"
+nintendo_ip = "10.42.0.147"
 nintendo_port = 8080
-
 nds_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 nds_client.connect((nintendo_ip, nintendo_port))
 
@@ -17,13 +16,23 @@ def set_nth_bit(n):
 mask_a_key = set_nth_bit(0)
 
 while True:
+    # wysyłamy dowolny znak w ramach synchronizacji
     nds_client.send('A'.encode('ascii'))
-    packet = nds_client.recv(2) #otrzymujemy pakiet z naszymi klawiszami
-    keys_held_little_endian = struct.unpack('<H', packet) #'H' oznacza unsigned short, '<' oznacza little endian
-    print("Received as bytes : " + str(bin(keys_held_little_endian[0])))
-    print("Received as integer : " + str(keys_held_little_endian[0]))
-    key_a_pressed = (keys_held_little_endian[0] & mask_a_key) != 0
-    print("Masked 1st bit - A key - " + str((key_a_pressed)))
-
+    # teraz otrzymujemy pakiet z naszymi klawiszami:
+    packet = nds_client.recv(2)
+    if len(packet) == 0:
+        print("NDS zamknął połączenie")
+        nds_client.close()
+        break
+    # odkodowanie otrzymanych bitów w taki sposób,
+    # aby ich kolejność była taka sama jak na NDS.
+    # 'H' oznacza unsigned short, '<' oznacza little endian
+    keys_held_little_endian = struct.unpack('<H', packet)[0]
+    print("Otrzymane bajty : " + str(bin(keys_held_little_endian)))
+    print("Otrzymane bajty jako liczba : " + str(keys_held_little_endian))
+    key_a_pressed = (keys_held_little_endian & mask_a_key) != 0
+    print("Czy wciśnięto A - " + str((key_a_pressed)))
     if key_a_pressed:
         pyautogui.press('space')
+
+print("Koniec")
