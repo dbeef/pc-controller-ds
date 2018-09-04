d80584572c98140450788fba47ab3514ec51eda2
d.zalega <daniel.zalega@gmail.com>
2018-09-04 19:46:39 +0200

    Adding comments.

--- nintendo-server/source/main.cpp
+++ nintendo-server/source/main.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <nds/arm9/console.h>
 #include <nds/arm9/input.h>
+#include <sys/socket.h>
 
 #define SSID "feebdx"
 #define SSID_LEN 6
@@ -13,82 +14,75 @@
 #define PORT 8080
 
 char ack{};
-int stat;
 
-void receive_ack(int &stat, int &socket) {
-    do {
-        stat = recv(socket, &ack, sizeof(ack), 0);
-        if (stat == 1) {
-            break;
-        }
-    } while (stat < 0);
-
-};
+int receive_ack(int socket) {
+    return recv(socket, &ack, sizeof(ack), 0);
+}
 
 void receive_transmission() {
 
-    printf("[*] Setting up server:\n");
+    printf("[*] Trwa konfiguracja serwera:\n");
 
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1; //True, dla re-używania portu i adresu
     int addrlen = sizeof(address);
 
-    printf("[*] Setting socket file descriptor\n");
+    printf("[*] Ustawiam deskryptor pliku\n");
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("[-] Setting socket file descriptor failed\n");
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("[-] Deskryptor pliku nie zostal ustawiony\n");
         exit(EXIT_FAILURE);
     }
 
-    printf("[*] Setting socket options\n");
+    printf("[*] Ustawiam konfiguracje socketu \n");
 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("[-] Setting socket options\n");
+        perror("[-] Nie udalo sie ustawic konfiguracji\n");
         exit(EXIT_FAILURE);
     }
 
-    printf("[+] Done setting socket\n");
+    printf("[+] Ustawiono opcje\n");
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    printf("[*] Binding socket to port %i\n", PORT);
+    printf("[*] Wiazanie socketu z portem %i\n", PORT);
 
     if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
-        perror("[-] Binding socket failed");
+        perror("[-] Nie udalo sie zwiazac socketu");
         exit(EXIT_FAILURE);
     }
 
-    printf("[+] Done binding socket to port %i\n", PORT);
-
-    printf("[*] Listening for connections\n");
+    printf("[*] Zwiazano socket z portem, nasluchiwane polaczen\n");
 
     if (listen(server_fd, 3) < 0) {
-        perror("[*] Error on listening for connection\n");
+        perror("[*] Blad podczas nasluchiwania \n");
         exit(EXIT_FAILURE);
     }
-
-    printf("[*] Accepting connection\n");
 
     if ((new_socket = accept(server_fd, (struct sockaddr *) &address, &addrlen)) < 0) {
-        perror("[-] Failed accepting connection\n");
+        perror("[-] Blad podczas nawiazywania polaczenia\n");
         exit(EXIT_FAILURE);
     }
 
-    printf("Entering loop: \n");
+    printf("[*] Nawiazano polaczenie\n");
 
     for (;;) {
-        receive_ack(stat, new_socket);
+        receive_ack(new_socket);
         scanKeys();
-        u16 keys_held = keysHeld();
-
-        do {
-            stat = send(new_socket, &keys_held, sizeof(u16), 0);
-        } while (stat < 0);
-
+        uint16 keys_held = keysHeld();
+        send(new_socket, &keys_held, sizeof(uint16), 0);
     }
+
+    shutdown(new_socket, SHUT_WR);
+    closesocket(new_socket);
+    //ponieważ closesocket zadziała asynchronicznie, to musimy poczekać, aby upewnić się, że pakiet
+    //RST (zamknięcie połączenia) został wysłany i pythonowy skrypt dowie się, że połączenie zostało zerwane -
+    //inaczej od razu przejdziemy do wywołania return 0;
+    //częstotliwość odświeżania ekranu NDS to 60Hz, co da 60 * (1/60) = 1s delayu
+    for (int frames = 0; frames < 24; frames++) { swiWaitForVBlank(); }
     //koniec funkcji receive_transmission
 }
 
@@ -101,7 +95,7 @@ int main() {
     strncpy(our_ap->ssid, SSID, SSID_LEN); //Ustawiamy nazwę sieci
     our_ap->ssid_len = SSID_LEN; //Ustawiamy długość nazwy sieci
     our_ap->channel = 1; //Ustawiamy kanał - należy pamiętać, żeby zgadzał się z tym od sieci
-    memset(our_ap->bssid, 0, 6); //Ustawiamy BSSID na zera, aby biblioteka go zignorowała
+    memset(our_ap->bssid, 0, 6); //Zerujemy BSSID, aby biblioteka go zignorowała
 
     printf("Connecting to %s\n", our_ap->ssid);
 
@@ -122,7 +116,7 @@ int main() {
         printf("\nip: [%li.%li.%li.%li]\n", (ip) & 0xFF, (ip >> 8) & 0xFF, (ip >> 16) & 0xFF, (ip >> 24) & 0xFF);
         receive_transmission(); //Nasza funkcja otwierająca socket
     } else {
-        printf("\nConnection failed!\n");
+        printf("\nNie udalo sie polaczyc!\n");
     }
     return 0;
 }
